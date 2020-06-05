7e7baafd63b7dd4ded8cc4cbe655e43e59a169ac
Asmaa Hosam <asmaahosam2009@gmail.com>
2020-06-05 22:30:36 +0200

    Initial Commit

---
+++ Assets/Scripts/Activate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate : MonoBehaviour
+{
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        canvas.gameObject.transform.LookAt(Camera.main.transform);
+        canvas.gameObject.transform.rotation = Quaternion.Euler(0, canvas.gameObject.transform.rotation.eulerAngles.y, 0);
+    }
+}
